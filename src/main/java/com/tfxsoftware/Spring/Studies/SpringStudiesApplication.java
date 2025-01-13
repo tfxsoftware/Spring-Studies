04fdd0d3870bf6899a8f3ed779120752ee6b633a
tfxsoftware <aswespeak.tfx@gmail.com>
2025-01-13 13:02:41 -0300

    feat: add logger to main application

--- src/main/java/com/tfxsoftware/Spring/Studies/SpringStudiesApplication.java
+++ src/main/java/com/tfxsoftware/Spring/Studies/SpringStudiesApplication.java
@@ -1,13 +1,16 @@
 package com.tfxsoftware.Spring.Studies;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringStudiesApplication {
-
+	private static final Logger logger = LoggerFactory.getLogger(SpringStudiesApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(SpringStudiesApplication.class, args);
+		logger.info("SpringStudiesApplication started");
 	}
 
 }
