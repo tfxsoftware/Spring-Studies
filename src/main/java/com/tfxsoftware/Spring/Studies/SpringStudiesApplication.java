a187029b134a106823d1279b84558b60dd0ae8b8
tfxsoftware <aswespeak.tfx@gmail.com>
2025-01-13 12:40:31 -0300

    first commit

---
+++ src/main/java/com/tfxsoftware/Spring/Studies/SpringStudiesApplication.java
@@ -0,0 +1,13 @@
+package com.tfxsoftware.Spring.Studies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tfxsoftware/Spring/Studies/SpringStudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfxsoftware.Spring.Studies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
